6e3229d35392b8563ea860c1a4b8dc2f19831551
NOS CREATOR <152608930+noscreator@users.noreply.github.com>
2024-01-06 23:06:59 +0530

    Update .install.sh

--- themes/bash/.install.sh
+++ themes/bash/.install.sh
@@ -10,13 +10,13 @@ PUT 11 40
 echo -e "\e[92mBy\e[1;93m Nos Creator\e[1;95m /\e[1;96m NOSCREATOR0 "
 PUT 12 3
 echo ""
-echo -e "  \e[101;1;39mNOTE\e[0;1;33m Use upto 9 words\e[0m"
+echo -e "\e[101;1;39mNOTE\e[0;1;33m Use upto 9 words\e[0m"
 echo ""
-cd ~/ninjaos-termux/.object
+cd ~/ninjaos-termux/themes/bash
 echo -e '\e[1;96m'
 read -p '  Type Banner Name ❯ ' name
-sed -e "s/\PROC/$name/g" .2zshrc > $HOME/.zshrc
+sed -e "s/\PROC/$name/g" .installzshrc > $HOME/.zshrc
 echo  ""
 echo -e '\e[1;96m'
 read -p '  Type Shell Name ❯ ' name
-sed -e "s/\NOSCREATOR/$name/g" .NOSCREATOR.zsh-theme > $HOME/.oh-my-zsh/themes/NOSCREATOR.zsh-theme
+sed -e "s/\NOSCREATOR/$name/g" .ninjaos.zsh-theme > $HOME/.oh-my-zsh/themes/ninjaos.zsh-theme
