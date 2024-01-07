cb28ecfd1d8403ce8b069333ef8064651d32cb48
NOS CREATOR <152608930+noscreator@users.noreply.github.com>
2024-01-07 15:56:00 +0530

    Update play_music.sh

--- bin/play_music.sh
+++ bin/play_music.sh
@@ -1,6 +1,10 @@
-if command -v play > /dev/null; then
-echo -e "\n${GREEN}Music playback started in the background.${NC}"
-play ./data/1704523429064_track.mp3 &
-else
-echo -e "\n${GREEN}Music playback is not available. Skipping...${NC}"
-fi
+#!/bin/bash
+
+# Path to your music file
+MUSIC_FILE="./data/1704523429064_track.mp3"
+
+# Check if mpg123 is installed
+command -v play >/dev/null 2>&1 || { echo >&2 "player is not installed. Aborting."; exit 1; }
+
+# Play music in the background
+play -q "$MUSIC_FILE" &
