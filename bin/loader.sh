4b004ee08e6c0306c358116a408145dcae1f2605
NOS CREATOR <152608930+noscreator@users.noreply.github.com>
2024-01-07 16:47:05 +0530

    Update loader.sh

--- bin/loader.sh
+++ bin/loader.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# Function to get the center position of the terminal
+get_center_position() {
+  local term_width
+  term_width=$(tput cols)
+  local center_position=$((term_width / 2))
+  echo $center_position
+}
+
 # Function for the loading animation
 loading_animation() {
+  local center_position
+  center_position=$(get_center_position)
+
   spinner="/|\\-/|\\-"
   while true; do
     for i in $(seq 0 7); do
-      echo -ne "\e[1;95m\r[${spinner:$i:1}] Loading... \e[0m"
+      printf "\e[1;95m%*s\e[0m\r" $center_position "[${spinner:$i:1}] Loading..."
       sleep 0.1
     done
   done
